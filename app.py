faa3464d4497267758edab53343b15853e71de10
bobruk76 <vladimir.m.polyakov@gmail.com>
2020-08-19 16:20:47 +0300

    algoritm end

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
-from service import get_word
+from service import get_word, letter_in_string
 
-set_word = ''
-result_word = ''
 def main():
+    points = 0
     set_word = get_word()
     result_word = '_' * len(set_word)
-    while True:
+    print('{} -- {}'.format(set_word, result_word))
+    while set_word != result_word and points < 4:
         letter = input("Введите букву:")
-        print('{} -- {}'.format(set_word,result_word))
+        if letter in set_word:
+            result_word = letter_in_string(letter, set_word, result_word)
+        else:
+            points += 1
+
+        print('{} Штрафных очков:{}'.format(result_word, points))
 
 if __name__ == '__main__':
     main()
--- service.py
+++ service.py
@@ -1,6 +1,12 @@
 import random
 
-
 def get_word():
     word_list = ['skillfactory', 'testing', 'blackbox', 'pytest', 'unittest', 'coverage']
     return random.choice(word_list)
+
+def letter_in_string(letter, string, res_string):
+    res = ''
+    for i, l in enumerate(string):
+        res += string[i] if string[i] == letter else res_string[i]
+    return res
+
